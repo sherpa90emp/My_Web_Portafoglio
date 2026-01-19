6a77093c60c2b2f6d56a2a2adc620a697bc48045
Andrea <chiappi.andrea@gmail.com>
2026-01-19 07:11:58 +0100

    v 0.12.26 - aggiunti metodi getByOrder controller + jd

--- backend/src/main/java/com/example/backend_my_web_portafoglio/controller/UscitaController.java
+++ backend/src/main/java/com/example/backend_my_web_portafoglio/controller/UscitaController.java
@@ -5,10 +5,7 @@ import com.example.backend_my_web_portafoglio.service.UscitaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -53,5 +50,56 @@ public class UscitaController {
         List<UscitaDTO> usciteDTO = uscitaService.findByDataSpesaBetween(dataInizio, dataFine);
         if (usciteDTO.isEmpty()) return ResponseEntity.noContent().build();
         return ResponseEntity.ok(usciteDTO);
+    }
+
+    /**
+     * Recupera tutte le uscite nel db ordinate in base a un campo e a una direzione specifica.
+     *
+     * @param campo il campo su cui ordinare le uscite
+     *              Valori ammessi: {@code dataSpesa}, {@code importo}
+     * @param ordine la direzione di ordinamento
+     *               Valori ammessi: {@code asc}, {@code desc}
+     *
+     * @return una ResponseEntity che contiene:
+     * - 200 OK con la lista di tutte le uscite ordinate
+     * - 204 No Content se la lista è vuota, ovvero non esistono uscite
+     * - 400 Bad Request se il mapping è errato
+     */
+    @GetMapping("/{campo}/{ordine}")
+    public ResponseEntity<List<UscitaDTO>> getAllUsciteOrder(
+            @PathVariable String campo,
+            @PathVariable String ordine
+    ) {
+        List<UscitaDTO> uscitaDTO;
+
+        boolean asc = "asc".equalsIgnoreCase(ordine);
+        boolean desc = "desc".equalsIgnoreCase(ordine);
+
+        if (!asc && !desc) return ResponseEntity.badRequest().build();
+
+        switch (campo) {
+            case "dataSpesa":
+                if (asc) {
+                    uscitaDTO = uscitaService.getAllUsciteOrderByDataAsc();
+                } else {
+                    uscitaDTO = uscitaService.getAllUsciteOrderByDataDesc();
+                }
+                break;
+            case "importo":
+                if (asc) {
+                    uscitaDTO = uscitaService.getAllUsciteOrderByImportoAsc();
+                } else {
+                    uscitaDTO = uscitaService.getAllUsciteOrderByImportoDesc();
+                }
+                break;
+            default:
+                return ResponseEntity.badRequest().build();
+        }
+
+        if (uscitaDTO.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(uscitaDTO);
     }
 }
