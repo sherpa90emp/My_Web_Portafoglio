e374b2dffd36b7e2eebb2c0a8f474017938157c5
Andrea <chiappi.andrea@gmail.com>
2026-02-02 07:03:59 +0100

    v 0.12.32 - aggiunta interface e metodo get AllT

--- frontend-my-web-portafoglio/src/app/service/allTransaction-service/all-transaction-service.ts
+++ frontend-my-web-portafoglio/src/app/service/allTransaction-service/all-transaction-service.ts
@@ -1,8 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+export interface AllTransactionDTO {
+  id: number;
+  importo: number;
+  descrizione: string;
+  data: string;
+  categoria: string;
+  tipoTransazione: string;
+}
 @Injectable({
   providedIn: 'root',
 })
 export class AllTransactionService {
-  
+  private apiUrl = "https://localhost:8081/api/alltransaction";
+
+  constructor(private http: HttpClient) {}
+
+  getAllTransaction(): Observable<AllTransactionDTO[]> {
+    return this.http.get<AllTransactionDTO[]>(this.apiUrl)
+  }
 }
