f838c95dc95b6cc5d47de26f92b233315d1f8ed0
Andrea <chiappi.andrea@gmail.com>
2026-02-02 07:25:55 +0100

    v 0.12.34 - aggiunta route sommario

--- frontend-my-web-portafoglio/src/app/app.routes.ts
+++ frontend-my-web-portafoglio/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePage } from './components/home-page/home-page';
 import { Tabella } from './components/tabelle/tabella-uscite/tabella-uscite';
 import { TabellaEntrate } from './components/tabelle/tabella-entrate/tabella-entrate';
+import { TabellaAllTransaction } from './components/tabelle/tabella-all-transaction/tabella-all-transaction';
 
 export const routes: Routes = [
   { path: '', component: HomePage, title: 'Home' },
   { path: 'uscite', component: Tabella, title: 'Tabella Uscite' },
   { path: 'entrate', component: TabellaEntrate, title: 'Tabella Entrate' },
+  { path: 'sommario', component: TabellaAllTransaction, title: 'Tabella AllTransaction'},
 ];
