413694eb88483217c704b88d007e93796eb3d2ff
Andrea <chiappi.andrea@gmail.com>
2026-03-05 07:17:01 +0100

    v 0.12.57.1 - aggiunto metodo getAllEntrate...Paginate + jd

--- backend/src/main/java/com/example/backend_my_web_portafoglio/service/EntrataService.java
+++ backend/src/main/java/com/example/backend_my_web_portafoglio/service/EntrataService.java
@@ -3,6 +3,10 @@ package com.example.backend_my_web_portafoglio.service;
 import com.example.backend_my_web_portafoglio.mapper.EntrataMapper;
 import com.example.backend_my_web_portafoglio.model.dto.EntrataDTO;
 import com.example.backend_my_web_portafoglio.repository.EntrataRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.example.backend_my_web_portafoglio.model.entity.Entrata;
@@ -98,5 +102,19 @@ public class EntrataService {
      */
     public BigDecimal getSommaTotaleEntrate() {
         return entrataRepository.getSommaTotaleImportiEntrate();
+    }
+
+    /**
+     * Recupera una pagina di entrate presenti convertite in DTO, ordinate per data in ordine decrescente.
+     *
+     * @param numeroPagina numero della pagina da recuperare
+     * @param quantitaInPagina quantità massima di Entrate presenti per singola pagina
+     * @return un oggetto {@link Page} contenente la {@link EntrataDTO} disponibili ordinate per data e metadati di paginazione.
+     */
+    public Page<EntrataDTO> getAllEntrateOrderByDataDescPaginate(int numeroPagina, int quantitaInPagina) {
+        Pageable pageable = PageRequest.of(numeroPagina, quantitaInPagina, Sort.by("dataEntrata").descending());
+
+        return entrataRepository.findAllByPage(pageable)
+                .map(entrataMapper::toDTO);
     }
 }
