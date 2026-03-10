2707f7fb8060dc94aedbac777cdca33e53be28ff
Andrea <chiappi.andrea@gmail.com>
2026-03-10 07:11:46 +0100

    v 0.12.65.1 - unificati metodi getAllEntrate e getEntratePaginate in getPaginaEntrate

--- backend/src/main/java/com/example/backend_my_web_portafoglio/controller/EntrataController.java
+++ backend/src/main/java/com/example/backend_my_web_portafoglio/controller/EntrataController.java
@@ -37,48 +37,17 @@ public class EntrataController {
         return ResponseEntity.ok(entrateDTO);
     }
 
-    /**
-     * Recupera tutte le entrate nel db ordinate in base a un campo e a una direzione specifica.
-     *
-     * @param campo il campo su cui ordinare le entrate
-     *              Valori ammessi: {@code dataEntrata}, {@code importo}
-     * @param ordine la direzione di ordinamento
-     *               Valori ammessi: {@code asc}, {@code desc}
-     *
-     * @return una ResponseEntity che contiene:
-     * - 200 OK con la lista di tutte le entrate ordinate
-     * - 204 No Content se la lista è vuota, ovvero non esistono entrate
-     * - 400 Bad Request se il campo o la direzione di ordinamento non sono tra quelli permessi.
-     */
-    @GetMapping("/{campo}/{ordine}")
-    public ResponseEntity<List<EntrataDTO>> getAllEntrateOrder(
-            @PathVariable @Pattern(regexp = "dataEntrata|importo") String campo,
-            @PathVariable @Pattern(regexp = "asc|desc") String ordine
-    ) {
-        List<EntrataDTO> entrateDTO = entrataService.getAllEntrateOrderBy(campo, ordine);
-
-        return entrateDTO.isEmpty()
-                ? ResponseEntity.noContent().build()
-                : ResponseEntity.ok(entrateDTO);
-    }
-
-    /**
-     * Recupera una pagina di entrate dal db, ordinate per data decrescente.
-     *
-     * @param numeroPagina l'indice della pagina da recuperare
-     * @param quantitaPerPagina il numero massimo di elementi per pagina.
-     * @return una ResponseEntity che contiene:
-     * - 200 OK con la pagina contenente le entrate
-     * - 400 Bad Request se i parametri di paginazione non sono validi
-     * - 204 No Content se la pagina è vuota, ovvero non ci sono entrate
-     */
     @GetMapping("/page")
     public ResponseEntity<Page<EntrataDTO>> getPaginaEntrate(
             @RequestParam(defaultValue = "0") @Min(0) int numeroPagina,
-            @RequestParam(defaultValue = "10") @Min(1) int quantitaPerPagina
+            @RequestParam(defaultValue = "10") @Min(1) int quantitaPagina,
+            @RequestParam(defaultValue = "dataEntrata") @Pattern(regexp = "dataEntrata|importo") String campo,
+            @RequestParam(defaultValue = "desc") @Pattern(regexp = "asc|desc") String ordine
     ) {
-        Page<EntrataDTO> entrataDTOPage = entrataService.getAllEntrateOrderByDataDescPaginate(numeroPagina, quantitaPerPagina);
-        if (entrataDTOPage.isEmpty()) return ResponseEntity.noContent().build();
-        return ResponseEntity.ok(entrataDTOPage);
+        Page<EntrataDTO> entrataDTOPage = entrataService.getAllEntratePaginateOrderBy(numeroPagina, quantitaPagina, campo, ordine);
+
+        return entrataDTOPage.isEmpty()
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.ok(entrataDTOPage);
     }
 }
