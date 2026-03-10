7f702b8fe86bcd6360a099a6d70b80c231bfd87c
Andrea <chiappi.andrea@gmail.com>
2026-03-10 06:49:37 +0100

    v 0.12.63.1 - rimozione metodi custum, presenti già in JPAR

--- backend/src/main/java/com/example/backend_my_web_portafoglio/repository/EntrataRepository.java
+++ backend/src/main/java/com/example/backend_my_web_portafoglio/repository/EntrataRepository.java
@@ -1,9 +1,6 @@
 package com.example.backend_my_web_portafoglio.repository;
 
 import com.example.backend_my_web_portafoglio.model.entity.Entrata;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
