ea9dda1b89be3688353150d3188ffe3def814a3e
Andrea <chiappi.andrea@gmail.com>
2026-03-13 19:23:09 +0100

    v 0.12.70.1 - unificati metodi in getPaginaUscite

--- backend/src/main/java/com/example/backend_my_web_portafoglio/controller/UscitaController.java
+++ backend/src/main/java/com/example/backend_my_web_portafoglio/controller/UscitaController.java
@@ -2,13 +2,13 @@ package com.example.backend_my_web_portafoglio.controller;
 
 import com.example.backend_my_web_portafoglio.model.dto.UscitaDTO;
 import com.example.backend_my_web_portafoglio.service.UscitaService;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -36,93 +36,27 @@ public class UscitaController {
     }
 
     /**
-     * Recupera tutte le uscite nel db comprese in un intervallo di tempo specifico.
-     *
-     * @param dataInizio data di inizio dell'intervallo
-     * @param dataFine   data di fine dell'intervallo
-     * @return una ResponseEntity che contiene:
-     * - 200 OK con la lista di tutte le uscite comprese nell'intervallo di tempo
-     * - 204 No Content se la lista è vuota, ovvero non esistono uscite in quell'intervallo di tempo
-     */
-    @GetMapping("/periodo")
-    public ResponseEntity<List<UscitaDTO>> getUsciteDataBetween(
-            @RequestParam("inizio") @DateTimeFormat(pattern = "yyyy-MM-dd") Date dataInizio,
-            @RequestParam("fine") @DateTimeFormat(pattern = "yyyy-MM-dd") Date dataFine) {
-        List<UscitaDTO> usciteDTO = uscitaService.findByDataSpesaBetween(dataInizio, dataFine);
-        if (usciteDTO.isEmpty()) return ResponseEntity.noContent().build();
-        return ResponseEntity.ok(usciteDTO);
-    }
-
-    /**
-     * Recupera tutte le uscite nel db ordinate in base a un campo e a una direzione specifica.
-     *
-     * @param campo il campo su cui ordinare le uscite
-     *              Valori ammessi: {@code dataSpesa}, {@code importo}
-     * @param ordine la direzione di ordinamento
-     *               Valori ammessi: {@code asc}, {@code desc}
-     *
-     * @return una ResponseEntity che contiene:
-     * - 200 OK con la lista di tutte le uscite ordinate
-     * - 204 No Content se la lista è vuota, ovvero non esistono uscite
-     * - 400 Bad Request se il mapping è errato
-     */
-    @GetMapping("/{campo}/{ordine}")
-    public ResponseEntity<List<UscitaDTO>> getAllUsciteOrder(
-            @PathVariable String campo,
-            @PathVariable String ordine
-    ) {
-        List<UscitaDTO> uscitaDTO;
-
-        boolean asc = "asc".equalsIgnoreCase(ordine);
-        boolean desc = "desc".equalsIgnoreCase(ordine);
-
-        if (!asc && !desc) return ResponseEntity.badRequest().build();
-
-        switch (campo) {
-            case "dataSpesa":
-                if (asc) {
-                    uscitaDTO = uscitaService.getAllUsciteOrderByDataAsc();
-                } else {
-                    uscitaDTO = uscitaService.getAllUsciteOrderByDataDesc();
-                }
-                break;
-            case "importo":
-                if (asc) {
-                    uscitaDTO = uscitaService.getAllUsciteOrderByImportoAsc();
-                } else {
-                    uscitaDTO = uscitaService.getAllUsciteOrderByImportoDesc();
-                }
-                break;
-            default:
-                return ResponseEntity.badRequest().build();
-        }
-
-        if (uscitaDTO.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-
-        return ResponseEntity.ok(uscitaDTO);
-    }
-
-    /**
-     * Recupera una pagina di uscite dal db, ordinate per data decrescente.
+     * Recupera una pagina di uscite dal db, ordinate in base ai parametri forniti.
      *
      * @param numeroPagina l'indice della pagina da recuperare
      * @param quantitaPerPagina Il numero massimo di elementi per pagina.
+     * @param campo il parametro sulla base del quale effettuare l'ordinamento
+     * @param ordine direzione dell'ordinamento, può essere ascendente o decrescente
      * @return una ResponseEntity che contiene:
      * - 200 OK con la pagina contenente le uscite
-     * - 400 Bad Request se i parametri di paginazione non sono validi
      * - 204 No Content se la pagina è vuota, ovvero non ci sono uscite
      */
     @GetMapping("/page")
     public ResponseEntity<Page<UscitaDTO>> getPaginaUscite(
-            @RequestParam(defaultValue = "0") int numeroPagina,
-            @RequestParam(defaultValue = "10") int quantitaPerPagina
+            @RequestParam(defaultValue = "0") @Min(0) int numeroPagina,
+            @RequestParam(defaultValue = "10") @Min(1) int quantitaPerPagina,
+            @RequestParam(defaultValue = "dataEntrata") @Pattern(regexp = "dataEntrata|importo") String campo,
+            @RequestParam(defaultValue = "desc") @Pattern(regexp = "asc|desc") String ordine
     ) {
-        if (numeroPagina < 0 || quantitaPerPagina <= 0) return ResponseEntity.badRequest().build();
+        Page<UscitaDTO> uscitaDTOPage = uscitaService.getAllUscitePaginateOrderBy(numeroPagina, quantitaPerPagina, campo, ordine);
 
-        Page<UscitaDTO> uscitaDTOPage = uscitaService.getAllUsciteOrderByDataDescPaginate(numeroPagina, quantitaPerPagina);
-        if (uscitaDTOPage.isEmpty()) return ResponseEntity.noContent().build();
-        return ResponseEntity.ok(uscitaDTOPage);
+        return uscitaDTOPage.isEmpty()
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.ok(uscitaDTOPage);
     }
 }
