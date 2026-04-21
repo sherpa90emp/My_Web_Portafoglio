814e22d57a130090ae1f1be816547597eebe16a2
Andrea <chiappi.andrea@gmail.com>
2026-04-21 06:58:10 +0200

    v 0.12.78.6 - refactor MovimentoDTO + jd

--- backend/src/main/java/com/example/backend_my_web_portafoglio/model/dto/MovimentoDTO.java
+++ backend/src/main/java/com/example/backend_my_web_portafoglio/model/dto/MovimentoDTO.java
@@ -2,6 +2,7 @@ package com.example.backend_my_web_portafoglio.model.dto;
 
 import com.example.backend_my_web_portafoglio.model.entity.Entrata;
 import com.example.backend_my_web_portafoglio.model.entity.Uscita;
+import com.example.backend_my_web_portafoglio.model.enums.TipoTransazione;
 import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
@@ -11,7 +12,7 @@ import java.util.Date;
  * Data Transfer Object nato dall'unione delle entità {@link Uscita} e {@link Entrata}.
  * Utilizzato per trasferire i dati tra i vari livelli dell'app senza esporre le entità originali.
  */
-public class AllTransactionDTO {
+public class MovimentoDTO {
     /**
      * Identificativo univoco della transazione, corrispondente all'ID dell'entità di origine ({@link Entrata} o {@link Uscita}).
      */
@@ -19,33 +20,33 @@ public class AllTransactionDTO {
     private String id;
 
     /**
-     * Importo in entrata o in uscita della transazione.
+     * Importo in entrata o in uscita del movimento.
      */
     @NotNull(message = "L'importo è obbligatorio")
     @PositiveOrZero(message = "L'importo deve essere positivo")
     private BigDecimal importo;
 
     /**
-     * Descrizione della transazione.
+     * Descrizione del movimento.
      */
     private String descrizione;
 
     /**
-     * Data in cui è avvenuta la transazione.
+     * Data in cui è avvenuto il movimento.
      */
     @NotNull(message = "La data è obbligatoria")
     @PastOrPresent(message = "La data non può essere nel futuro")
     private Date data;
 
     /**
-     * Categoria associata alla transazione.
+     * Categoria associata al movimento.
      *
      * È valorizzata solo nel caso di una {@link Uscita} vale {@code null} se la transazione è una {@link Entrata}.
      */
     private String categoria;
 
     /**
-     * Indica se la transazione è un'entrata o un'uscita.
+     * Indica se il movimento è un'entrata o un'uscita.
      */
     @NotBlank(message = "Necessario specificare il tipo di transazione")
     private TipoTransazione tipo;
@@ -53,7 +54,7 @@ public class AllTransactionDTO {
     /**
      * Costruttore vuoto per inizializzare il DTO senza valori.
      */
-    public AllTransactionDTO() {
+    public MovimentoDTO() {
     }
 
     ;
@@ -62,13 +63,13 @@ public class AllTransactionDTO {
      * Costruttore usato per inizializzare il DTO con tutti i valori disponibili.
      *
      * @param id          identificativo univoco
-     * @param importo     importo della transazione
-     * @param descrizione descrizione associata alla transazione
-     * @param data        data in cui è avvenuta o è stata registrata la transazione
-     * @param categoria   categoria della transazione
-     * @param tipo        tipo di transazione
+     * @param importo     importo del movimento
+     * @param descrizione descrizione associata al movimento
+     * @param data        data in cui è avvenuto o è stato registrato il movimento
+     * @param categoria   categoria del movimento
+     * @param tipo        tipo di movimento
      */
-    public AllTransactionDTO(String id, BigDecimal importo, String descrizione, Date data, String categoria, TipoTransazione tipo) {
+    public MovimentoDTO(String id, BigDecimal importo, String descrizione, Date data, String categoria, TipoTransazione tipo) {
         this.id = id;
         this.importo = importo;
         this.descrizione = descrizione;
