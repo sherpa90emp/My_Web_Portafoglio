ad8f5398750b2a88a58bce2c956e405e5cc5a1fc
Andrea <chiappi.andrea@gmail.com>
2026-04-23 07:31:05 +0200

    v 0.12.80.6 - aggiunto getPaginaEntrate()

--- frontend-my-web-portafoglio/src/app/service/entrata-service/entrata.ts
+++ frontend-my-web-portafoglio/src/app/service/entrata-service/entrata.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Page } from '../page-model/page';
 
 export interface EntrataDTO {
   id: number;
@@ -25,5 +26,19 @@ export class EntrataService {
       ordine: 'asc' | 'desc'
     ): Observable<EntrataDTO[]> {
       return this.http.get<EntrataDTO[]>(`${this.apiUrl}/${campo}/${ordine}`)
+    }
+
+    getPaginaEntrate(
+      page: number, 
+      size: number,
+      campo: string,
+      ordine: 'asc' | 'desc'
+    ): Observable<Page<EntrataDTO>> {
+      const params = new HttpParams()
+        .set('numeroPagina', page)
+        .set('quantitaPagina', size)
+        .set('campo', campo)
+        .set('ordine', ordine);
+      return this.http.get<Page<EntrataDTO>>(this.apiUrl, { params });
     }
 }
