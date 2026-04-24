1ad5a9c048d7d224c93434c46063182747d213dd
Andrea <chiappi.andrea@gmail.com>
2026-04-24 07:23:26 +0200

    v 0.12.83.6 - tolto input; aggiunti metodi per la paginazione entrate

--- frontend-my-web-portafoglio/src/app/components/tabelle/tabella-entrate/tabella-entrate.ts
+++ frontend-my-web-portafoglio/src/app/components/tabelle/tabella-entrate/tabella-entrate.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EntrataDTO, EntrataService } from '../../../service/entrata-service/entrata';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Arrow } from '../../arrow/arrow';
 import { Subject, switchMap } from 'rxjs';
+import { Page } from '../../../service/page-model/page';
 
 @Component({
   selector: 'app-tabella-entrate',
@@ -11,18 +12,39 @@ import { Subject, switchMap } from 'rxjs';
   styleUrl: './tabella-entrate.css',
 })
 export class TabellaEntrate implements OnInit {
-  @Input() entrate: EntrataDTO[] = [];
+  entrate: EntrataDTO[] = [];
+  
+  paginaEntrate: Page<EntrataDTO> | null = null;
+  numeroPagina = 0;
+  quantitaPagina = 20;
+  campo: 'dataEntrata' | 'importo' = 'dataEntrata';
+  ordine: 'asc' | 'desc' = 'desc';
+
+
 
   constructor(private entrataService: EntrataService) {}
 
   private sort$ = new Subject<{ campo: 'dataEntrata' | 'importo'; direzione: 'asc' | 'desc' }>();
 
   ngOnInit(): void {
+    this.caricaEntratePaginate();
+  }
+
+  ordina(campo: 'dataEntrata' | 'importo', direzione: 'asc' | 'desc') {
+    this.campo = campo;
+    this.ordine = direzione;
+    this.numeroPagina = 0;
+    this.caricaEntratePaginate();
+  }
+
+  caricaEntrate(): void {
     this.entrataService.getEntrate().subscribe({
       next: (data) => (this.entrate = data),
       error: (error) => console.error('Errore nel caricamento dei dati', error),
     });
+  }
 
+  caricaEntrateOrdinate() : void {
     this.sort$
       .pipe(
         switchMap(({ campo, direzione }) =>
@@ -32,7 +54,26 @@ export class TabellaEntrate implements OnInit {
       .subscribe((data) => (this.entrate = data));
   }
 
-  ordina(campo: 'dataEntrata' | 'importo', direzione: 'asc' | 'desc') {
-    this.sort$.next({ campo, direzione });
+  caricaEntratePaginate(): void {
+    this.entrataService
+      .getPaginaEntrate(this.numeroPagina, this.quantitaPagina, this.campo, this.ordine)
+      .subscribe({
+        next: (data) => (this.paginaEntrate = data),
+        error: (error) => console.error('Errore nel caricamento dei dati', error),
+      })
+  }
+
+  paginaSuccessiva(): void {
+    if (!this.paginaEntrate?.last) {
+      this.numeroPagina++;
+      this.caricaEntratePaginate();
+    }
+  }
+
+  paginaPrecendente(): void {
+    if (!this.paginaEntrate?.first) {
+      this.numeroPagina--;
+      this.caricaEntratePaginate();
+    }
   }
 }
