3b7a033d3c56d126db37f22bc9c5543b8638e092
Andrea <chiappi.andrea@gmail.com>
2026-04-28 17:44:03 +0200

    v 0.12.96.8 - mod metodi; aggiunti metodi paginazione

--- frontend-my-web-portafoglio/src/app/components/tabelle/tabella-uscite/tabella-uscite.ts
+++ frontend-my-web-portafoglio/src/app/components/tabelle/tabella-uscite/tabella-uscite.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { UscitaDTO, UscitaService } from '../../../service/uscita-service/uscita';
 import { Arrow } from '../../arrow/arrow';
 import { Subject, switchMap } from 'rxjs';
 import { Paginazione } from "../../paginazione/paginazione";
+import { Page } from '../../../service/page-model/page';
 
 @Component({
   selector: 'app-tabella',
@@ -14,16 +15,35 @@ import { Paginazione } from "../../paginazione/paginazione";
 export class Tabella implements OnInit {
   uscite: UscitaDTO[] = [];
 
+  paginaUscite: Page<UscitaDTO> | null = null;
+  numeroPagina = 0;
+  quantitaPagina = 18;
+  campo: 'dataSpesa' | 'importo' = 'dataSpesa';
+  ordine: 'asc' | 'desc' = 'desc';
+
   constructor(private uscitaService: UscitaService) {}
 
   private sort$ = new Subject<{ campo: 'dataSpesa' | 'importo'; direzione: 'asc' | 'desc' }>();
 
   ngOnInit(): void {
+    this.caricaUscitePaginate();    
+  }
+
+  ordina(campo: 'dataSpesa' | 'importo', direzione: 'asc' | 'desc') {
+    this.campo = campo;
+    this.ordine = direzione;
+    this.numeroPagina = 0;
+    this.caricaUscitePaginate();
+  }
+
+  caricaUscite(): void {
     this.uscitaService.getUscite().subscribe({
       next: (data) => (this.uscite = data),
       error: (error) => console.error('Errore nel caricamento dei dati', error),
     });
+  }
 
+  caricaUsciteOrdinate(): void {
     this.sort$
       .pipe(
         switchMap(({ campo, direzione }) => this.uscitaService.getUsciteOrdinate(campo, direzione)),
@@ -31,7 +51,12 @@ export class Tabella implements OnInit {
       .subscribe((data) => (this.uscite = data));
   }
 
-  ordina(campo: 'dataSpesa' | 'importo', direzione: 'asc' | 'desc') {
-    this.sort$.next({ campo, direzione });
+  caricaUscitePaginate(): void {
+    this.uscitaService
+      .getUscitePaginate(this.numeroPagina, this.quantitaPagina, this.campo, this.ordine)
+      .subscribe({
+        next: (data) => (this.paginaUscite = data),
+        error: (error) => console.error('Errore nel caricamento dei dati', error),
+      });
   }
 }
