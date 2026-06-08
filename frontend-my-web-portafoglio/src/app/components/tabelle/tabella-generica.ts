3ed1d55f0dcf8e1276ae301a8d9e6ca17d0062f3
Andrea <chiappi.andrea@gmail.com>
2026-06-08 07:34:02 +0200

    v 0.12.114.16 - creato ts tabella generica

---
+++ frontend-my-web-portafoglio/src/app/components/tabelle/tabella-generica.ts
@@ -0,0 +1,92 @@
+import { AfterViewInit, Directive, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Page } from '../../service/page-model/page';
+import { Observable } from 'rxjs';
+
+@Directive()
+export abstract class TabellaGenerica<T> implements OnInit, AfterViewInit, OnDestroy {
+  paginaData: Page<T> | null = null;
+  numeroPagina = 0;
+  quantitaPagina = 18;
+  ordine: 'asc' | 'desc' = 'desc';
+
+  abstract campo: string;
+
+  @ViewChild('containerTabella') containerTabella!: ElementRef;
+
+  private resizeObserver: ResizeObserver | null = null;
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.initResizeObserver();
+    this.caricaDati();
+  }
+
+  ngOnDestroy(): void {
+    this.destroyResizeObserver();
+  }
+
+  abstract fetchDati(
+    page: number,
+    size: number,
+    campo: string,
+    ordine: 'asc' | 'desc',
+  ): Observable<Page<T>>;
+
+  caricaDati(): void {
+    this.fetchDati(this.numeroPagina, this.quantitaPagina, this.campo, this.ordine).subscribe({
+      next: (data) => (this.paginaData = data),
+      error: (error) => console.error('Errore nel caricamento dei dati', error),
+    });
+  }
+
+  ordina(campo: string, direzione: 'asc' | 'desc') {
+    this.campo = campo;
+    this.ordine = direzione;
+    this.numeroPagina = 0;
+    this.caricaDati();
+  }
+
+  paginaSuccessiva(): void {
+    if (!this.paginaData?.last) {
+      this.numeroPagina++;
+      this.caricaDati();
+    }
+  }
+
+  paginaPrecendente(): void {
+    if (!this.paginaData?.first) {
+      this.numeroPagina--;
+      this.caricaDati();
+    }
+  }
+
+  calcolaQuantitaPagina(): number {
+    const altezzaTabella = this.containerTabella.nativeElement.clientHeight;
+    console.log('altezza tabella con resize: ', altezzaTabella);
+    const altezzaRiga = 41;
+    return Math.max(10, Math.floor(altezzaTabella / altezzaRiga));
+  }
+
+  initResizeObserver(): void {
+    this.resizeObserver = new ResizeObserver(() => {
+      const nuovaQuantita = this.calcolaQuantitaPagina();
+      if (nuovaQuantita !== this.quantitaPagina) {
+        this.quantitaPagina = nuovaQuantita;
+        this.numeroPagina = 0;
+        this.caricaDati();
+      }
+    });
+  
+    if (this.containerTabella?.nativeElement) {
+      this.resizeObserver.observe(this.containerTabella.nativeElement);
+    }
+  }
+
+  destroyResizeObserver(): void {
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect();
+      this.resizeObserver = null;
+    }
+  }
+}
